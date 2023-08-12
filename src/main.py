c93bbe5a3ef77c49241ee275870abbe371aa25f5
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-08-12 15:41:38 -0400

    Refactored to OOD Principles

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
-from tts import read_guided_meditation
+from tts import MeditationPlayer
 
 def main():
+    # Create an instance of the MeditationPlayer class
+    player = MeditationPlayer()
+
     # Read the meditation text from the file
     with open("data/sample_meditation.txt", "r") as file:
         meditation_text = file.read()
 
-    # Call the function to read the guided meditation
-    read_guided_meditation(meditation_text)
+    # Call the method to play the guided meditation
+    player.play_meditation(meditation_text)
 
 if __name__ == "__main__":
     main()
--- src/tts.py
+++ src/tts.py
@@ -4,65 +4,80 @@ from playsound import playsound
 import os
 import glob
 
-def read_guided_meditation(meditation_text):
-    
-    # Clean up any existing .mp3 files from previous runs
-    existing_files = glob.glob("temp_*.mp3")
-    for audio_file in existing_files:
-        try:
-            os.remove(audio_file)
-        except OSError as e:
-            print(f"Error deleting {audio_file}: {e}")
 
-    # Split the input by lines
-    meditation_lines = [line for line in meditation_text.strip().split("\n") if " - " in line]
+class MeditationPlayer:
+    def __init__(self):
+        self.last_timestamp = 0
+        self.start_time = None
+        self.file_counter = 0
 
-    last_timestamp = 0
-    start_time = None  # To track the start of the meditation
+    def _cleanup_audio_files(self):
+        """Remove temporary audio files."""
+        existing_files = glob.glob("temp_*.mp3")
+        for audio_file in existing_files:
+            try:
+                os.remove(audio_file)
+            except OSError as e:
+                print(f"Error deleting {audio_file}: {e}")
 
-    file_counter = 0
+    def _split_lines(self, meditation_text):
+        """Split the meditation text into actionable lines."""
+        return [line for line in meditation_text.strip().split("\n") if " - " in line]
 
-    # Pre-generate the first line
-    _, content = meditation_lines[0].split(" - ")
-    filename_next = f"temp_{file_counter}.mp3"
-    gTTS(text=content, lang='en').save(filename_next)
-
-    for i in range(len(meditation_lines)):
-        line = meditation_lines[i]
-        
-        timestamp_str, content = line.split(" - ")
+    def _get_timestamp(self, line):
+        """Extract the timestamp from a line."""
+        timestamp_str, _ = line.split(" - ")
         minutes, seconds = map(int, timestamp_str.split(":"))
-        timestamp = minutes * 60 + seconds
-        
-        filename_current = filename_next
-        file_counter += 1
+        return minutes * 60 + seconds
 
-        # Generate next line if it exists
-        if i + 1 < len(meditation_lines):
-            _, next_content = meditation_lines[i + 1].split(" - ")
-            filename_next = f"temp_{file_counter}.mp3"
-            gTTS(text=next_content, lang='en').save(filename_next)
+    def _generate_audio(self, content):
+        """Generate audio file from text."""
+        filename = f"temp_{self.file_counter}.mp3"
+        gTTS(text=content, lang='en').save(filename)
+        self.file_counter += 1
+        return filename
 
-        # Start the timer if not started
-        if not start_time:
-            start_time = time.time()
+    def play_meditation(self, meditation_text):
+        self._cleanup_audio_files()
+        meditation_lines = self._split_lines(meditation_text)
 
-        # Check internal timer against timestamp
-        elapsed_time = time.time() - start_time
-        while elapsed_time < timestamp:
-            time.sleep(0.1)
-            elapsed_time = time.time() - start_time
+        # Pre-generate the first line
+        _, content = meditation_lines[0].split(" - ")
+        filename_next = self._generate_audio(content)
 
-        print(f"Internal Timer: {elapsed_time:.2f} seconds")
+        for i in range(len(meditation_lines)):
+            line = meditation_lines[i]
+            timestamp = self._get_timestamp(line)
+            _, content = line.split(" - ")
 
-        # Play the sound
-        playsound(filename_current)
-        os.remove(filename_current)
+            filename_current = filename_next
 
-        last_timestamp = timestamp
+            # Generate next line if it exists
+            if i + 1 < len(meditation_lines):
+                _, next_content = meditation_lines[i + 1].split(" - ")
+                filename_next = self._generate_audio(next_content)
 
-    end_time = time.time()
-    duration = end_time - start_time
-    print(f"Meditation took {duration:.2f} seconds.")
+            # Start the timer if not started
+            if not self.start_time:
+                self.start_time = time.time()
+
+            # Check internal timer against timestamp
+            elapsed_time = time.time() - self.start_time
+            while elapsed_time < timestamp:
+                time.sleep(0.1)
+                elapsed_time = time.time() - self.start_time
+
+            print(f"Internal Timer: {elapsed_time:.2f} seconds")
+
+            # Play the sound
+            playsound(filename_current)
+            os.remove(filename_current)
+
+            self.last_timestamp = timestamp
+
+        end_time = time.time()
+        duration = end_time - self.start_time
+        print(f"Meditation took {duration:.2f} seconds.")
+
 
 
