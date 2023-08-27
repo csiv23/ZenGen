ff4a3090ce6ef84f46e885437d2ba9e38d8ff85f
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-08-27 15:37:36 -0400

    Added environment variable support in backend

--- backend/app.py
+++ backend/app.py
@@ -1,44 +1,63 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, request, render_template, jsonify, send_from_directory
 from flask_cors import CORS
 from main import generate_meditation_text
 
+# Load environment variables from .env file
+load_dotenv()
+
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/')
+# Retrieve the frontend URL from environment variable
+# (default to 'http://localhost:3000' if not found)
+FRONTEND_URL = os.environ.get("FRONTEND_URL", "http://localhost:3000")
+
+# Configure CORS for the app
+CORS(app, resources={r"/*": {"origins": FRONTEND_URL}})
+
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/test', methods=['GET'])
+
+@app.route("/test", methods=["GET"])
 def test_route():
     return "Test route is working!"
 
-@app.route('/generate_meditation', methods=['POST'])
+
+@app.route("/generate_meditation", methods=["POST"])
 def generate_meditation():
     try:
         print("Received data: ")
         print(request.form)
 
-        length_choice = request.form['length_choice']
-        focus_choice = request.form['focus_choice']
-        method_choice = request.form['method_choice']
-        
-        meditation_text = generate_meditation_text(length_choice, focus_choice, method_choice)
-        print(meditation_text)  
-        
+        length_choice = request.form["length_choice"]
+        focus_choice = request.form["focus_choice"]
+        method_choice = request.form["method_choice"]
+
+        meditation_text = generate_meditation_text(
+            length_choice, focus_choice, method_choice
+        )
+        print(meditation_text)
+
         return jsonify({"success": True, "meditation_text": meditation_text})
 
     except Exception as e:
         return jsonify({"success": False, "error": str(e)})
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def catch_all(path):
-    return send_from_directory('frontend/build', 'index.html')
+    return send_from_directory("frontend/build", "index.html")
 
-@app.route('/<path:filename>')
+
+@app.route("/<path:filename>")
 def serve_static(filename):
-    return send_from_directory('frontend/build', filename)
+    return send_from_directory("frontend/build", filename)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
